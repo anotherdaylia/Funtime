12b499e067674a9a1539e6dd7019c5e18b64850d
anotherdaylia <anotherdaylia@gmail.com>
2016-01-28 20:10:26 -0800

    creating project

---
+++ src/main/java/com/lia/App.java
@@ -0,0 +1,13 @@
+package com.lia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
